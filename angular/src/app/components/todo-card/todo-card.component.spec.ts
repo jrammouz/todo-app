750244c3ba0a761a6d3f9b39d95cf82172a80177
Ramy Sobhieh <Rami.sobhieh@partner.bmwgroup.com>
2024-04-23 19:23:39 +0300

    Add test for action buttons

--- angular/src/app/components/todo-card/todo-card.component.spec.ts
+++ angular/src/app/components/todo-card/todo-card.component.spec.ts
@@ -2,6 +2,7 @@ import {fireEvent, getByTestId, render, screen} from '@testing-library/angular';
 import {TodoCardComponent} from "./todo-card.component";
 import {TodosService} from "../../services/todos.service";
 import {of} from "rxjs";
+import createSpy = jasmine.createSpy;
 
 
 describe('TodoCardComponent', () => {
@@ -16,7 +17,7 @@ describe('TodoCardComponent', () => {
       "completed": false,
       "userId": 15
     })
-  )
+  );
 
   todosService.deleteTodo.and.returnValue(
     of({
@@ -27,7 +28,7 @@ describe('TodoCardComponent', () => {
       "isDeleted": true,
       "deletedOn": "2024-04-21T14:00:27.095Z"
     })
-  )
+  );
 
 
   it('should call markAsComplete when checkbox is checked', async () => {
@@ -66,28 +67,55 @@ describe('TodoCardComponent', () => {
     expect(checkbox.checked).toBeTrue();
   })
 
-  // it('should emit on delete trash button click', async () => {
-  //
-  //   await render(TodoCardComponent, {
-  //     providers:[{provide : TodosService, useValue : todosService}],
-  //     componentInputs: {
-  //       todo : {
-  //         id: '5',
-  //         title : 'make your bed',
-  //         content :'please make your bed'
-  //       },
-  //       isSelected : false
-  //     },
-  //     componentOutputs:{F
-  //       // removeCardFromList:{
-  //       //   emit:
-  //       // } as any
-  //     }
-  //   })
-  //
-  //   const button = screen.getByTestId('deleteBtn') as HTMLInputElement;
-  //   fireEvent.click(button); // Check the checkbox
-  //   // expect().toHaveBeenCalled();
-  // });
+  it('should show action buttons when delete icon click, then delete when delete btn clicked', async () => {
+    const emit = createSpy();
+      await render(TodoCardComponent, {
+      providers:[{provide : TodosService, useValue : todosService}],
+      componentInputs: {
+        todo : {
+          id: '5',
+          title : 'make your bed',
+          content :'please make your bed'
+        },
+        isSelected : false
+      },
+        componentOutputs:{
+        removeCardFromList:{
+          emit : emit
+        } as any
+        }
+    })
+    const trash = screen.getByTestId('delete-icon') as HTMLInputElement;
+
+    fireEvent.click(trash); // click on the trash icon
+    expect(screen.queryByTestId('delete-icon')).toBeNull();
+    const deleteBtn = screen.getByTestId('delete-btn') as HTMLInputElement;
+    fireEvent.click(deleteBtn);
+    expect(emit).toHaveBeenCalledWith('5');
+  })
+
+  it('should show action buttons when delete icon click, then reshow trash icon when cancel btn clicked', async () => {
+    await render(TodoCardComponent, {
+      providers:[{provide : TodosService, useValue : todosService}],
+      componentInputs: {
+        todo : {
+          id: '5',
+          title : 'make your bed',
+          content :'please make your bed'
+        },
+        isSelected : false
+      },
+    })
+    const trash = screen.getByTestId('delete-icon') as HTMLInputElement;
+
+    fireEvent.click(trash); // click on the trash icon
+    expect(screen.queryByTestId('delete-icon')).toBeNull();
+    const cancelBtn = screen.getByTestId('close-btn') as HTMLInputElement;
+    fireEvent.click(cancelBtn);
+    expect(screen.queryByTestId('delete-icon')).toBeTruthy();
+    expect(screen.queryByTestId('close-btn')).toBeNull();
+    expect(screen.queryByTestId('delete-btn')).toBeNull();
+
+  })
 
 })
