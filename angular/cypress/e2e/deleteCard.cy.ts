b1863b8288121bfe6bf3d4662cb412e30856d3ac
Charbel Abou Khalil <charbel.aboukhalil@partner.bmwgroup.com>
2024-04-24 00:01:58 +0300

    add integrations test

--- angular/cypress/e2e/deleteCard.cy.ts
+++ angular/cypress/e2e/deleteCard.cy.ts
@@ -1,10 +1,23 @@
 describe("Delete Card", () => {
-  it("deletes a todo card from list", () => {
-    cy.visit('/')
-
-    const card = cy.findAllByText('30');
-    let deleteIcon = card.get("#delete-icon")
-    deleteIcon.click()
-    cy.findAllByText('30').should('not.exist');
+  beforeEach(() => {
+    cy.visit('/');
   })
+
+  it("deletes a todo card from list", () => {
+    const card = cy.findByText('30');
+    const deleteIcon = card.get("#delete-icon")
+    deleteIcon.click()
+    const deleteConfirmation = card.get("#confirmed-delete");
+    deleteConfirmation.click();
+    cy.findByText('30').should('not.exist');
+  });
+
+  it("show details modal on click", () => {
+    const card = cy.findByText('30');
+    card.click()
+
+
+    const modal = cy.get('[data-testid="iammodal"]');
+    modal.should('exist');
+  });
 })
